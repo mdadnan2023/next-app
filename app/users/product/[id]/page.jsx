1c43fae56c2ff426ed7711ec45052eb49fcf8900
Md Adnan <md.adnan@viaconteam.com>
2024-06-21 11:12:57 +0530

    detail page

--- app/users/product/[id]/page.jsx
+++ app/users/product/[id]/page.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-
-const getProduct = async () => {
-    const res= await fetch("https://jsonplaceholder.typicode.com/posts" +id,);
-    const posts = await res.json();
+async function getProduct(id) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts" + id, {
+    next: {
+      revalidate: 60,
+    },
+  });
+  return res.json();
 }
 
 export default async function ProductCardDetail({ params }) {
   const post = await getProduct(params.id);
 
   return (
-    <div className="card">
-      <p className="text-[black]">{post.title}</p>
-    </div>
+    <main>
+      <nav>
+        <h2>Product Detail</h2>
+      </nav>
+      <div className="card">
+        <p className="text-[black]">{post.title}</p>
+      </div>
+    </main>
   );
 }
